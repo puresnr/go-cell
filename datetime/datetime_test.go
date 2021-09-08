724527e97f2b8b06ddff209665c1ccff9f297f90
孤云独去闲 <912198800@qq.com>
2021-09-09 01:09:10 +0800

    Update datetime_test.go
    增加ToWeekdayISO()的测试

--- datetime/datetime_test.go
+++ datetime/datetime_test.go
@@ -19,3 +19,10 @@ func TestLawAge(t *testing.T) {
 	assert.Equal(t, LawAge("2021-09-09 15:00:00",time.Now().Format(TimeFormatDatetime)), InvalidAge)
 	assert.Equal(t, LawAge("2021-09-09","2022-09-08"), 0)
 }
+
+func TestToWeekdayISO(t *testing.T) {
+	weekdays := []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday, time.Saturday, time.Sunday}
+	for idx, w := range weekdays {
+		assert.Equal(t, idx + 1, ToWeekdayISO(w))
+	}
+}
